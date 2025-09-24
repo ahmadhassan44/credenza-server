184a82dab9af87d21829cae3fa6119bc0556d3ad
Ahmad Hassan <ahmadhassantanveer44@gmail.com>
2025-09-24 18:15:57 +0500

    feat/add https server config

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger, ValidationPipe } from '@nestjs/common';
+import * as fs from 'fs';
 
 async function bootstrap() {
   const logger = new Logger('Bootstrap');
-  const app = await NestFactory.create(AppModule);
+
+  let app;
+  const isProd = process.env.NODE_ENV === 'production';
+
+  if (isProd) {
+    // Load SSL certs
+    const httpsOptions = {
+      key: fs.readFileSync('/etc/ssl/selfsigned/selfsigned.key'),
+      cert: fs.readFileSync('/etc/ssl/selfsigned/selfsigned.crt'),
+    };
+
+    app = await NestFactory.create(AppModule, { httpsOptions });
+    logger.log('Starting in HTTPS (production) mode');
+  } else {
+    app = await NestFactory.create(AppModule);
+    logger.log('Starting in HTTP (development) mode');
+  }
 
   // Configure global prefix for all routes
   app.setGlobalPrefix('api/v1');
@@ -19,4 +36,5 @@ async function bootstrap() {
   await app.listen(port);
   logger.log(`Application listening on port ${port}`);
 }
+
 bootstrap();
